bcd01a4de91373d73565dc6972229b25022027a1
Heinrich Bernard <moneytalk130195@gmail.com>
2023-10-12 12:23:19 +0700

    Remove provider and add validation in salon middleware

--- src/SalonMiddleware.php
+++ src/SalonMiddleware.php
@@ -4,20 +4,29 @@ namespace Lakuuu\SalonMiddleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class SalonMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        $context = [];
+        $data = [];
 
         if (array_key_exists('LAMBDA_REQUEST_CONTEXT', $_SERVER)) {
             $context = json_decode($_SERVER['LAMBDA_REQUEST_CONTEXT'], true);
+            $data = Arr::get($context, 'authorizer.lambda');
+        }
+
+        if (env('env') === 'local') {
+            $data = [
+                'user_id' => $request->header('user_id'),
+                'salon_id' => $request->header('salon_id')
+            ];
         }
 
         $request->merge([
-            'user_id' => $context['user_id'] ?? '',
-            'salon_id' => $context['salon_id'] ?? '',
+            'user_id' => $data['user_id'] ?? '',
+            'salon_id' => $data['salon_id'] ?? '',
         ]);
 
         return $next($request);

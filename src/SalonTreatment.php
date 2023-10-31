c741be05872f04d2f14d4548bbc233a238d1153c
Heinrich Bernard <moneytalk130195@gmail.com>
2023-10-31 15:05:34 +0700

    First commit

---
+++ src/SalonTreatment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Lakuuu\SalonApi;
+
+use Illuminate\Support\Facades\Http;
+
+class SalonTreatment
+{
+    private $url;
+
+    public function __construct()
+    {
+        $this->url = env(
+            'LAKUUU_SALON_TREATMENT',
+            'https://staging-salon-api.lakuuu.click/api/v1/treatment-service'
+        );
+    }
+
+    public function getAllTreatment($salonId)
+    {
+        return Http::withHeaders(['salon_id' => $salonId])->get($this->url.'/treatment');
+    }
+}

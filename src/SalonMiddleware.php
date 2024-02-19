7a823ea7785dbaa6d77b076a3e085aa7ab30fe77
Heinrich Bernard <ben@lakuuu.id>
2024-02-19 17:44:39 +0700

    Added: - App Lakuuu Authorizer Service

--- src/SalonMiddleware.php
+++ src/SalonMiddleware.php
@@ -5,6 +5,7 @@ namespace Lakuuu\SalonMiddleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
 
 class SalonMiddleware
 {
@@ -15,6 +16,19 @@ class SalonMiddleware
         if (array_key_exists('LAMBDA_REQUEST_CONTEXT', $_SERVER)) {
             $context = json_decode($_SERVER['LAMBDA_REQUEST_CONTEXT'], true);
             $data = Arr::get($context, 'authorizer.lambda');
+        }
+
+        if (env('APP_LAKUUU_AUTHORIZER_URL') !== '') {
+            $userRequest = Http::post(env('APP_LAKUUU_AUTHORIZER_URL'), [
+                'access_token' => $request->header('Authorization')
+            ]);
+            $userRequest->throw();
+            $userResponse = $userRequest->json();
+
+            $data = [
+                'user_id' => $userResponse['id'],
+                'salon_id' => $userResponse['salon_id'],
+            ];
         }
 
         if (env('APP_ENV') !== 'staging' && env('APP_ENV') !== 'production') {
